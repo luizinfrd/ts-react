9e3694745ac2898d6fd1277d4712612d05f3c7f4
Luis Felipe <111872373+luizinfrd@users.noreply.github.com>
2026-03-16 16:22:30 -0300

    Add files via upload

--- TestePage.tsx
+++ TestePage.tsx
@@ -1,33 +1,54 @@
 import React, { useState } from "react";
 
-interface CartaoUsuarioProps {
-    nome: string;
-    idade?: number;
+interface propsBuscaCaminho{
+    id: number;
+    email: string;
+    setor: string;
 }
 
-function CartaoUsuario({nome, idade}: CartaoUsuarioProps) {
-    const [botao, setBotao] = useState<boolean>(false);
+function BuscaCaminho(){
+    const [caminhos, setCaminhos] = useState<string>("");
+    const [resultadoBusca, setResultadoBusca] = useState<propsBuscaCaminho[]>([]);
+    
+    function handleBuscaCaminho(){
+        const request = fetch("http://localhost:8080/consignacao/posicao-produto/relatorio-posicao-produto?data=2024-06-01&produto=123456789")
+        .then(response => response.json())
+        .then((data: Array<propsBuscaCaminho>) => setResultadoBusca(data)) 
+        .catch(error => console.error("Erro na requisição:", error));
+    }
 
+    const dadosFalsos: propsBuscaCaminho[] = [
+    { id: 1, email: "joao@email.com", setor: "TI" },
+    { id: 2, email: "maria@email.com", setor: "RH" },
 
-    return (
+];
+
+    return(
         <div>
-            <h2>{nome}</h2>
-            <button onClick={() => setBotao(!botao)}>Clique aqui</button>
-            {botao && (
-                <p>Idade: {idade ?? "Não informada"}</p>
-            )}
-            {botao && <p>O botão foi clicado!</p>}
+            <input type="text" value={caminhos} onChange={(e) => setCaminhos(e.target.value)}/>
+            <input type="button" value="button" onClick={() => setResultadoBusca(dadosFalsos.filter(dado => dado.setor.includes(caminhos)))} />
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Email</th>
+                        <th>Setor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {resultadoBusca.map((dado) =>(
+                        <tr key={dado.id}>
+                            <td>{dado.id}</td>
+                            <td>{dado.email}</td>
+                            <td>{dado.setor}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-    );
+    )
 }
 
-const teste = <CartaoUsuario nome="João" idade={0} />;
+export default BuscaCaminho;
 
-export default function TestePage() {
-    return (
-        <div>
-            <h1>Teste de Componente</h1>
-            {teste}
-        </div>
-    );
-}
